03cf0c96a2aad4408b54aa36840c26b975bbbaa1
YashGupta-24 <yashgupta.241103@gmail.com>
2023-12-16 13:20:04 +0530

    script update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const s1 = document.querySelector("#screen1");
 const s2 = document.querySelector("#screen2");
+let g1 = document.querySelector("#guess1");
+let g2 = document.querySelector("#guess2");
+let g3 = document.querySelector("#guess3");
 function guessnum(count) {
     // console.log(`Player ${count} Guess a Number between 0-10`);
     // this.guessNum = prompt("Enter your guess: ");
@@ -21,44 +24,45 @@ function guessnum(count) {
     }
 }
 function compare() {
-    document.querySelector("#newGame").classList.remove("hide");
+    if (g1.value != "" && g2.value != "" && g3.value != "") {
+        document.querySelector("#newGame").classList.remove("hide");
 
-    document.querySelector(".submit").classList.add("hide");
-    
-    const guessNum = Math.floor(Math.random() * 11);
-    // const numPlayer1 = document.getElementById('guess1').value;
-    // const numPlayer2 = document.getElementById('guess2').value;
-    // const numPlayer3 = document.getElementById('guess3').value;
-    const numPlayer1 = guessnum(1);
-    const numPlayer2 = guessnum(2);
-    const numPlayer3 = guessnum(3);
+        document.querySelector(".submit").classList.add("hide");
 
+        const guessNum = Math.floor(Math.random() * 11);
+        // const numPlayer1 = document.getElementById('guess1').value;
+        // const numPlayer2 = document.getElementById('guess2').value;
+        // const numPlayer3 = document.getElementById('guess3').value;
+        const numPlayer1 = guessnum(1);
+        const numPlayer2 = guessnum(2);
+        const numPlayer3 = guessnum(3);
 
-    if (guessNum == numPlayer1) {
-        if (guessNum == numPlayer2 && guessNum == numPlayer3) {
-            s1.innerHTML = "Game Tie. Try Again";
-        } else if (guessNum == numPlayer2) {
-            s1.innerHTML = "Player 1 and Player 2 are winners!";
-        } else if (guessNum == numPlayer3) {
-            s1.innerHTML = "Player 1 and Player 3 are winners!";
-        } else {
-            s1.innerHTML = "Player 1 is the winner!";
+        if (guessNum == numPlayer1) {
+            if (guessNum == numPlayer2 && guessNum == numPlayer3) {
+                s1.innerHTML = "Game Tie. Try Again";
+            } else if (guessNum == numPlayer2) {
+                s1.innerHTML = "Player 1 and Player 2 are winners!";
+            } else if (guessNum == numPlayer3) {
+                s1.innerHTML = "Player 1 and Player 3 are winners!";
+            } else {
+                s1.innerHTML = "Player 1 is the winner!";
+            }
         }
-    }
-    else if (guessNum == numPlayer2) {
-        if (guessNum == numPlayer3) {
-            s1.innerHTML = "Player 2 and Player 3 are winners!";
-        } else {
-            s1.innerHTML = "Player 2 is the winner!";
+        else if (guessNum == numPlayer2) {
+            if (guessNum == numPlayer3) {
+                s1.innerHTML = "Player 2 and Player 3 are winners!";
+            } else {
+                s1.innerHTML = "Player 2 is the winner!";
+            }
         }
+        else if (guessNum == numPlayer3) {
+            s1.innerHTML = "Player 3 is the winner!";
+        }
+        else {
+            s1.innerHTML = "No one wins. Try again.";
+        }
+        s2.innerHTML = guessNum;
     }
-    else if (guessNum == numPlayer3) {
-        s1.innerHTML = "Player 3 is the winner!";
-    }
-    else {
-        s1.innerHTML = "No one wins. Try again.";
-    }
-    s2.innerHTML = guessNum;
 };
 
 
@@ -73,12 +77,12 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
 
-const newGame=()=>{
-    s1.innerText="";
-    s2.innerText="";
-    document.querySelector("#guess1").value="";
-    document.querySelector("#guess2").value="";
-    document.querySelector("#guess3").value="";
+const newGame = () => {
+    s1.innerText = "";
+    s2.innerText = "";
+    g1.value = "";
+    g2.value = "";
+    g3.value = "";
 
     document.querySelector("#newGame").classList.add("hide");
 
